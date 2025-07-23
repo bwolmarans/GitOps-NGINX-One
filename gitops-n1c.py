49ff2eb93955393b0ca96914e10867836aa781cf
bwolmarans <itsbrett@gmail.com>
2025-07-23 14:20:19 -0700

    qw

--- gitops-n1c.py
+++ gitops-n1c.py
@@ -83,6 +83,7 @@ if __name__ == '__main__':
     xxx = "Bearer APIToken " + xc_bearer_token
     print(xxx)
     print(n1c_namespace)
+    print("------------------")
 
     headers = {
         "Authorization": "Bearer APIToken u9Uh8k+YZtikEIwMI3BzMamS3uY=",
