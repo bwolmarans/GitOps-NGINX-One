7e798f6badce371d574e5740b757a11a3f392c44
bwolmarans <itsbrett@gmail.com>
2025-07-24 09:48:18 -0700

    ddd

--- gitops-n1c.py
+++ gitops-n1c.py
@@ -6,7 +6,7 @@
 # This script is intended to be run in a GitOps context, where it can be triggered by changes in a Git repository.
 # This is only intended to patch /etc/nginx/nginx.conf, but can be extended to patch other files.
 # This uses command line parameters to pass in the NGINX One Console hostname, namespace, instance ID, API token, and the base64 encoded NGINX configuration file.
-
+# use --help to get the command line options.
 
 
 
@@ -96,17 +96,16 @@ def n1c_check_publication_status(api_base_path, headers, nginx_instance_id, publ
 
     except requests.exceptions.RequestException as e:
         print("An error occurred:", e)
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='GitOps for NGINX One Console')
+    parser = argparse.ArgumentParser(description='Python example for using the NGINX One Console Rest API')
     parser.add_argument('--n1c_tenant_fqdn', help='The F5 Distribted Cloud Tenant FQDN', default='nginx-tenant1.staging.volterra.us')
     parser.add_argument('--n1c_namespace', help="The F5 Distribted Cloud n1c_namespace within your tenant", default='default')
-    parser.add_argument('--nginx_instance_id', help='The nginx instance uid from NGINX One Console', default='your NGINX instance ID here')
+    parser.add_argument('--nginx_instance_id', help='The nginx instance uid from NGINX One Console. Not needed if nginx instance hostname specified.', default='your NGINX instance ID here')
     parser.add_argument('--xc_api_token', help='The F5 Distribted Cloud API Token', default='your XC API Token here')
     parser.add_argument('--nginx_config_file', help='The /etc/nginx/nginx.conf file, base64d', default='your base64d nginx config here')  
-    parser.add_argument('--nginx_instance_hostname', help='The hostname of the NGINX instance', default='nothing here, use the instance id')
-    
+    parser.add_argument('--nginx_instance_hostname', help='The hostname of the NGINX instance.  If not provided will attempt to use nginx instance id.', default='nothing here, use the instance id')
     args = parser.parse_args()
-
 
     n1c_tenant_fqdn = args.n1c_tenant_fqdn
     n1c_namespace = args.n1c_namespace
