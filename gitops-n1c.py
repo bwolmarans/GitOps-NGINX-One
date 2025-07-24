c8aaffdffe8ca6f7b9e90a2ed6ad0da8e66c9189
bwolmarans <itsbrett@gmail.com>
2025-07-23 17:20:32 -0700

    qwe

--- gitops-n1c.py
+++ gitops-n1c.py
@@ -2,15 +2,9 @@ import requests
 import argparse
 import json
 import sys
-import os
-from urllib.parse import urljoin
-import pprint
-from requests.auth import HTTPBasicAuth
+from time import sleep
 
-#- name: check publication status1
-#run: |
-#    curl -X GET https://${{ vars.N1SC_HOSTNAME }}/api/nginx/one/n1c_namespaces/default/instances/${{ vars.N1SC_INSTANCE }}/publications/${{ env.PUBLICATION_ID }}  -H 'Authorization: Bearer APIToken ${{ secrets.xc_bearer_token }}' -o response.json
-#    cat response.json | jq -c .status
+
 def n1c_response_error_check(response_text):
     if "credential invalid" in response_text:
         print("ERROR: your API Authentication failed.  Check that your XC authentication token is")
@@ -19,30 +13,26 @@ def n1c_response_error_check(response_text):
         print(response_text)
         sys.exit(2) 
 
-
 def n1c_list_instances(api_base_path, headers):
     try:
         response = requests.get(api_base_path + "/instances", headers=headers)
-        # Check if the response was successful
         response_text = response.text
-        #print("Response received:", response_text)
         n1c_response_error_check(response_text)
         return response_text
-
     except requests.exceptions.RequestException as e:
         print("An error occurred:", e)
 
 def n1c_patch_nginx_config(api_base_path, headers, nginx_instance_id, payload):
     try:
         response = requests.patch(api_base_path + "/instances/" + nginx_instance_id + "/config" , headers=headers, json=payload)
-        # Check if the response was successful
         if response.status_code >= 200 and response.status_code < 300:
             response_text = response.text
             n1c_response_error_check(response_text)
-            print("Response received:", response_text)
-            # Check if the response contains the word "xxx"
-            #if "online" in response_text:
-            #   print("great")
+            data = json.loads(response_text)
+            publication_id = data['object_id']
+            publication_status = data['status']
+            return publication_id
+
         else:
             print(f"Something unexpected happened. The patch has probably failed. HTTP Status Code: {response.status_code}")
             print("Response:", response.text)
@@ -59,27 +49,39 @@ def n1c_get_nginx_config(api_base_path, headers, nginx_instance_id):
     except requests.exceptions.RequestException as e:
         print("An error occurred:", e)
 
+def n1c_check_publication_status(api_base_path, headers, nginx_instance_id, publication_id):
+    try:
+        response = requests.get(api_base_path + "/instances/" + nginx_instance_id + "/publications/" + publication_id, headers=headers)
+        if response.status_code >= 200 and response.status_code < 300:
+            response_text = response.text
+            n1c_response_error_check(response_text)
+            data = json.loads(response_text)
+            return data['status']
+        else:
+            print(f"Something unexpected happened. HTTP Status Code: {response.status_code}")
+            print("Response:", response.text)
 
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='GitOps for NGINX One Console')
     parser.add_argument('--n1c_hostname', help='The F5 Distribted Cloud Tenant FQDN', default='nginx-tenant1.staging.volterra.us')
     parser.add_argument('--n1c_namespace', help="The F5 Distribted Cloud n1c_namespace within your tenant", default='default')
     parser.add_argument('--nginx_instance_id', help='The nginx instance uid from NGINX One Console', default='xxxxxxxxxxxxx')
-    parser.add_argument('--xc_bearer_token', help='The F5 Distribted Cloud API Bearer Token', default='xxxxxxxxxxxxxxxx')
+    parser.add_argument('--xc_api_token', help='The F5 Distribted Cloud API Token', default='xxxxxxxxxxxxxxxx')
     parser.add_argument('--nginx_config_file', help='The nginx.conf, i.e. could come from the repo IaaC on push', default='xxxxxxxxxxxxxxxx')
     
     args = parser.parse_args()
 
 
-
     n1c_hostname = args.n1c_hostname
     n1c_namespace = args.n1c_namespace
     nginx_instance_id = args.nginx_instance_id
-    xc_bearer_token = args.xc_bearer_token
+    xc_api_token = args.xc_bearer_token
     nginx_config_file = args.nginx_config_file
     nginx_config_file_size = len(nginx_config_file)
 
-    auth_string = "Bearer APIToken " + xc_bearer_token
+    auth_string = " APIToken " + xc_api_token
     headers = {
         "Authorization": auth_string,
         "Accept": "application/json",
@@ -98,7 +100,13 @@ if __name__ == '__main__':
     #print(json.dumps(x, indent=2))
     #print("------------------------------------------------------------------------------------")
     
-    etc_nginx_nginx_dot_conf_payload = {"aux": [], "conf_path": "/etc/nginx/nginx.conf", "configs": [ { "files": [ { "contents": nginx_config_file, "mtime": "1970-01-01T00:00:00Z", "name": "nginx.conf", "size": nginx_config_file_size } ], "name": "/etc/nginx" } ] }
+    payload = {"aux": [], "conf_path": "/etc/nginx/nginx.conf", "configs": [ { "files": [ { "contents": nginx_config_file, "mtime": "1970-01-01T00:00:00Z", "name": "nginx.conf", "size": nginx_config_file_size } ], "name": "/etc/nginx" } ] }
     
     
-    n1c_patch_nginx_config(api_base_path, headers, nginx_instance_id, etc_nginx_nginx_dot_conf_payload)
+    publication_id = n1c_patch_nginx_config(api_base_path, headers, nginx_instance_id, payload)
+    sleep (5)  # Wait for the publication to be processed
+    print("Checking publication status for publication ID:", publication_id)
+    status = n1c_check_publication_status(api_base_path, headers, nginx_instance_id, publication_id)
+    print(status)
+
+    
